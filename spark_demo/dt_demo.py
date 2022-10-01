9d0b8fe0fde774f089f237aade40e2654bc46dce
hezhi <kevinvalentine@outlook.com>
2022-10-01 18:35:32 +0800

    mlflow

---
+++ spark_demo/dt_demo.py
@@ -0,0 +1,6 @@
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import StringIndexer, VectorAssembler
+from pyspark.ml.regression import RandomForestRegressor
+from pyspark.ml.evaluation import RegressionEvaluator
+
+filePath = "./data/sf-airbnb-clean.parquet"
